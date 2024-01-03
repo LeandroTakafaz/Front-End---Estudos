f5bbd1112c09f0961d03927e51db42e66a85a8b1
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2024-01-03 09:34:46 -0400

    Aula 2 - Consoles

---
+++ Fundamentos JS/Aula 2 - Consoles/index.js
@@ -0,0 +1,9 @@
+let nome = "TK"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+/* window.alert(nome)
+nome = window.prompt("Insira seu nome:")
+let confirm = window.confirm("Confirma essa ação?") */
