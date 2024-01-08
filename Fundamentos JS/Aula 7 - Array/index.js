b2e7ad239612bf797125f59b10fa6f144f571201
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2024-01-08 09:50:10 -0400

    Aula 7 - Array

---
+++ Fundamentos JS/Aula 7 - Array/index.js
@@ -0,0 +1,7 @@
+const listaDeCompras = ["Açucar", false]
+console.log(listaDeCompras)
+
+listaDeCompras[1] = "Mortadela"
+listaDeCompras[5] = 32
+listaDeCompras[7] = true
+console.log(listaDeCompras)
