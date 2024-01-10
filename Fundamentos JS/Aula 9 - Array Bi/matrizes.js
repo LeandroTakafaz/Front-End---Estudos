af0533e79d47ac3838d7d5c0554aa3261fcdb8b2
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2024-01-10 15:19:50 -0400

    Atualização

--- Fundamentos JS/Aula 9 - Array Bi/matrizes.js
+++ Fundamentos JS/Aula 9 - Array Bi/matrizes.js
@@ -20,6 +20,7 @@ console.log(arr[2][1])
 const matriz = [
     ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
     ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+    
     ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
   ]
   console.table(matriz)
